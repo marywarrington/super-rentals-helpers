37f5ef47cc352e373bdcdd8a715c378b8793bd88
Josh Overly <joshoverly@yahoo.com>
2016-03-28 12:16:18 -0700

    add save/delete functions, everything works

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -8,6 +8,11 @@ export default Ember.Component.extend({
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    delete(rental) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
     }
   }
 });
@@ -16,3 +21,13 @@ export default Ember.Component.extend({
 //when the show image button is clicked the image show action will run; this will set the isimageshowing from false to true. when this is true, the if statement in the hbs will display our image.
 
 //added the image hide function to the actions the action helper is located in the image tag, so that when the user clicks the image it will set the image showing state to false.
+
+
+//added the delete function to the actions.  when triggered, the delete action will display a confirmation pop-up. if confirmed it will send the action destroyRental upwards.
+
+// it is super important to remember that:
+//ONLY ROUTE HANDLERS MAY CREATE DELETE OR UPDATE OBJECTS IN THE DATA STORE!!!!!!
+
+//the component can't delete the object itself; it sends a notification of intent by calling the this.sendAction function.  In the function's argument we provide the name of the function on the template and route handler we want to activate, and the specific rental we want that to happen to.
+
+// ember follows the paradigm, data down, actions up.  data is passed downwards from the route, to the template, and into the component, actions get passed back up in a similar fashion.  So the command is sent up to the template and it must know how to handle that.  so we will call it on the same line that we use to call that component. 
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,18 @@ export default Ember.Route.extend({
   model() {  //the same as writing model: function()
     return this.store.findAll('rental'); //step 2: -this.store.findAll('rental') here we modify the route to find method.  we specify this.store to refer to the Firebase data store that we set up.  Then we run the findAll method with the argument rental; this instructs Ember Data to find all records fo the type rental in the store and return the to our application.  step 1: -return rentals- a method within an Ember class is called a hook.  Here is a model hook added to our index route handler.  the hook returns an array of hard coded property rentals
   },
+
+  actions: {
+    save3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
 
 //the index.hbs template will read this as the model property. model data in a route handler is available to the templates and components that correspond to that route.  The index.hbs can display this model data because the data is within its context.
@@ -24,3 +36,6 @@ export default Ember.Route.extend({
 //   {{/each}}
 // </ul>
 // so we are still looping through the model and assigning the variable name rental to it.  variable names on the left of the equals refer to the name used within the component; on the right is the object we're assigning that variable name to.
+
+
+//the save function is passed up from the save1 function on the route handler for the new rental, it gets sent with the params laid out in that function.  the save1 function is called with an action helper in a button on the new rental hbs page.  I think this gets passed up as a save2 function to the index hbs where save 2 is set to equal save 3. Which this function uses on the index source file. Not quite sure about it all. 
