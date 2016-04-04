6e0cf6788e6c5b39c83f810d0298040d40a9d887
Marika Allely and Mary Warrington <marika@>
2016-04-04 10:10:31 -0700

    add sortBy price computed property to city-tile

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -4,5 +4,8 @@ export default Ember.Component.extend({
 
   fullLocation: Ember.computed('city.name', 'city.country', function() {
     return this.get('city.name') + ', ' + this.get('city.country');
-  })
+  }),
+
+  sortBy: ['cost:asc'],
+  sortedRentals: Ember.computed.sort('city.rentals', 'sortBy')
 });
